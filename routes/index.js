808cd5551af43fc1aaad4284a66e2c9dd1d24d95
Griffin Hammer <grifhammer@gmail.com>
2015-12-08 11:48:39 -0500

    Added posts to user when either buff or nerf button is pressed.

--- routes/index.js
+++ routes/index.js
@@ -5,42 +5,65 @@ var router = express.Router();
 var mongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-    mongoClient.connect('mongodb://localhost:27017/buffornerf', function(error, db){
-        db.collection('heroes').find().toArray(function (error, result){
-            var numHeroes = result.length;
-            var thisIndex = Math.floor(Math.random() * numHeroes)
-            var heroes = result[thisIndex];
-            res.render('index', {heroes: heroes });
-        });
-    });
+router.get('/', function (req, res, next) {
 
     //index page should load random item
-    //1. Get all items from mongoDB
     //2. Get current user from mongoDB via req.ip
     //3. Find photos user hasnt voted on
-    //4. load all of the items from 3 to an array
-    //5. choose random item from the array and set it to a var
-    //6. res.render() the index view and send it the photo
 
 
 
+    mongoClient.connect('mongodb://localhost:27017/buffornerf', function(error, db){
+        //1. Get all items from mongoDB
+        db.collection('heroes').find().toArray(function (error, result){
+            db.collection('users').find({'ip': req.ip}).toArray(function (error, result){
 
-    
-
-    
-  //1. get ALL items
-  //2. Sort them by highest likes
-  //3. res.resnder the standings view and pass it the sorted photo array
-
+            });
+            var numHeroes = result.length;
+            //5. choose random item from the array and set it to a var
+            var thisIndex = Math.floor(Math.random() * numHeroes)
+            //4. load all of the items from 3 to an array
+            var heroes = result;
+            //6. res.render() the index view and send it the photo
+            res.render('index', {heroes: heroes[thisIndex] });
+        });
+    });
 });
 
 router.get('/standings', function(req, res, next){
+
+    //1. get ALL items
+    //2. Sort them by highest likes
+    //3. res.resnder the standings view and pass it the sorted photo array
     res.render('index', {title: 'Standings'});
 });
 
-router.post('*', function (req, res, next){
+router.post('/buff', function (req, res, next){
+    var heroId = req.body.heroId;
+    var updatedBuffVotes = req.body.buffVotes + 1;
+    mongoClient.connect('mongodb://localhost:27017/buffornerf', function(error, db){
+        db.collection('users').insertOne( {
+            ip: req.ip,
+            vote: "buff",
+            hero: heroId
+        });
+        res.redirect('../');
+    });
+    //This will run for all posted pages
+
+});
+
+router.post('/nerf', function (req, res, next){
+    var heroId = req.body.heroId;
+    var updatedBuffVotes = req.body.buffVotes + 1;
+    mongoClient.connect('mongodb://localhost:27017/buffornerf', function(error, db){
+        db.collection('users').insertOne( {
+            ip: req.ip,
+            vote: "nerf",
+            hero: heroId
+        });
+        res.redirect('../');
+    });
     //This will run for all posted pages
 
 });
