123e32319d690f3b751ff41860e763804b7f223b
Griffin Hammer <grifhammer@gmail.com>
2015-12-10 11:40:31 -0500

    Combined the code for serving the json into one function that each get route calls.

--- routes/api.js
+++ routes/api.js
@@ -14,7 +14,7 @@ var Hero = require('../models/heroes');
 var User = require('../models/users')
 
 
-function getJSON(res, model){
+function serveJson(res, model){
     model.find(function (err, result){
         if(err){
             console.log(err);
@@ -26,25 +26,11 @@ function getJSON(res, model){
 }
 
 router.get('/heroes/get', function (req, res, next){
-    Hero.find(function(err, heroesResult){
-        if (err){
-            console.log(err);
-        }
-        else{
-            res.json(heroesResult);
-        }
-    });
+    serveJson(res, Hero);
 });
 
 router.get('/users/get', function (req, res, next){
-    User.find(function (err, usersResult){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.json(usersResult);
-        }
-    })
+    serveJson(res, User);
 });
 
 module.exports = router;
