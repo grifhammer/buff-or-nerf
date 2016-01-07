25c1fe596836a57f8c533d1c3e0dfc69b475d11b
Griffin Hammer <grifhammer@gmail.com>
2016-01-07 17:24:57 -0500

    Further Refined Standings Page and added filters.

--- public/javascripts/standing-sort.js
+++ public/javascripts/standing-sort.js
@@ -15,6 +15,28 @@ $(document).ready(function(){
         }
     });
 
+    var filterFns = {
+        buff: function(){
+            var isBuff = $(this).find('h2.buff').text();
+            console.log(isBuff)
+            return isBuff;
+        },
+        nerf: function(){
+            var isNerf = $(this).find('h2.nerf').text();
+            return isNerf;
+        },
+        balanced:function() {
+            var isBalanced = $(this).find('h2.balanced').text();
+            return isBalanced;
+        }
+    }
+
+    $('.filter-btn-group').on('click', 'button', function(){
+        var filterValue = $(this).attr('data-filter');
+        filterValue = filterFns[filterValue] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+
     $('.sort-by-btn-group').on('click', 'button', function(){
         var sortValue = $(this).attr('data-sort-value');
         $grid.isotope({sortBy: sortValue});
